410eab6a4a42b1f9c21214387962f415cfef9ad9
Romulo Americo <116516646+Romulo-Americo@users.noreply.github.com>
2024-06-01 22:03:00 -0300

    Delete src directory

--- src/components/ActionsButtons.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './actionButtons.css'
-
-function ActionsButtons({color, description}){
-    return(
-        <button className='actionbuttons' style={{ backgroundColor: color }}>{description}</button>
-    );
-}
-
-export default ActionsButtons;
--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './button.css'
-
-function Button(prop){
-    return(
-        <button className='button'><a href="#">{prop.description}</a></button>
-    );
-}
-
-export default Button
